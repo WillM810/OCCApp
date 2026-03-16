d61382b3348929eff907d73eeae5d5c06aaf98c4
Will <mcvay.will@gmail.com>
2026-03-16 09:16:09 -0400

    wrap .json() call in try/catch

--- occ-app/src/lib/utils.ts
+++ occ-app/src/lib/utils.ts
@@ -1,5 +1,14 @@
+import { NextRequest } from "next/server";
+
 export function convertDateJIC(d: Date) {
     return (d.getMonth() + 1).toString().padStart(2, '0') +
             d.getDate().toString().padStart(2, '0') +
             d.getFullYear();
+}
+
+export async function safeJson(req: NextRequest) {
+    try {
+        return await req.json();
+    } catch {}
+    return {};
 }
