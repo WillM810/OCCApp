b59622bfd2456a07ba992497c4b5cd2572c20942
Will <mcvay.will@gmail.com>
2026-03-25 10:22:54 -0400

    read court contacts from file persistance

--- occ-app/src/lib/attorneyEmails.ts
+++ occ-app/src/lib/attorneyEmails.ts
@@ -1,26 +1,6 @@
 import { readFile } from "./filePersistance";
 
 export const attorneyData = readFile('contacts/attorneys.json');
-
-export const ccpEmails = [
-    '"Melanie Barry" <melanie.barry@delaware.gov>',
-    '"Julie Brooks" <julie.brooks@delaware.gov>',
-    '"Christine Syva" <christine.syva@delaware.gov>',
-];
-
-export const fcEmails = [
-    '"FC_Kent_Criminal (MailBox Resources)" <fc_kent_criminal@delaware.gov>',
-    '"Licausi, Patricia A" <patricia.licausi@delaware.gov>',
-    '"Resh, Ashely R" <ashely.r.resh@delaware.gov>',
-    '"Karen Whitney" <karen.whitney@delaware.gov>',
-    '"Rachel Tesoro" <rachel.tesoro@delaware.gov>',
-    '"Tiffany Cahall" <tiffany.cahall@delaware.gov>',
-];
-
-export const scEmails = [
-    '"Annette D Ashley" <Annette.Ashley@delaware.gov>',
-    '"Superior Kent Criminal Proth" <Superior_Kent_Proth_Criminal@delaware.gov>',
-    '"Kirra E Britt" <Kirra.Britt@delaware.gov>',
-    '"Jenna Fries" <Jenna.Fries@delaware.gov>',
-    '"Tracy L. Hitchens" <Tracy.Hitchens@delaware.gov>',
-];
+export const ccpEmails = readFile('contacts/ccp.json');
+export const fcEmails = readFile('contacts/fam.json');
+export const scEmails = readFile('contacts/sup.json');
