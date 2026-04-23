983a01373633703c5d81c9c535e366f199efaeb6
Will <mcvay.will@gmail.com>
2026-04-23 10:24:08 -0400

    file persistance data types

--- occ-app/src/lib/filePersistance.ts
+++ occ-app/src/lib/filePersistance.ts
@@ -1,15 +1,22 @@
 import fs from 'fs';
 
-export function readFile(file: string) {
+export type AttorneyData = {
+    barId: string;
+    name: string;
+    schedule: boolean;
+    emails: string[];
+}
+
+export function readDataFile(file: string): Object[] {
     try {
         return JSON.parse(fs.readFileSync(process.cwd() + '/data/' + file).toString('utf8'));
     } catch (e) {
         console.error(e);
-        return {};
+        return [];
     }
 }
 
-export function writeFile(file: string, data: Object) {
+export function writeFile(file: string, data: Object[]) {
     try {
         fs.writeFileSync(process.cwd() + '/data/' + file, JSON.stringify(data, null, 2));
     } catch (e) {
