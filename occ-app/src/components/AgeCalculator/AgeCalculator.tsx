2f63d3db909723fe940ba86851449d3cf81bafe3
Will <mcvay.will@gmail.com>
2026-05-13 10:35:07 -0400

    remove unneeded styles after age calc updates

--- occ-app/src/components/AgeCalculator/AgeCalculator.tsx
+++ occ-app/src/components/AgeCalculator/AgeCalculator.tsx
@@ -1,28 +1,3 @@
-import { ChangeEvent, useState } from "react";
-
-const dateInputStyles = [
-    "w-1/2",
-    "p-2",
-    "border",
-    "rounded-md",
-    "focus:outline-none",
-    "focus:ring-2",
-    "focus:ring-blue-500",
-    "bg-white",
-    "text-gray-900",
-    "placeholder-gray-500",
-    "border-gray-300",
-    "dark:bg-gray-800",
-    "dark:text-white",
-    "dark:placeholder-gray-400",
-    "dark:border-gray-600",
-    "scheme-light",
-    "dark:scheme-dark",
-    "dark:[&::-webkit-calendar-picker-indicator]:filter-none!",
-    "dark:[&::-webkit-calendar-picker-indicator]:invert",
-    "dark:[&::-webkit-calendar-picker-indicator]:brightness-125"
-].join(' ');
-
 export default function AgeCalculator() {
     return (
         <div className="w-2/5 flex items-center space-x-4">
